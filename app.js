372cd05c07c57873d68eff43b12c673e3811df7f
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-03-29 14:54:21 -0700

    adding middleware

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 // Require Libraries
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 // App Setup
 const app = express();
 
 // Middleware
+const hbs = handlebars.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo() { return 'FOO!'; },
+        bar() { return 'BAR!'; }
+    }
+});
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-  });
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+  })
+
+  app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+  })
 
 // Start Server
 
